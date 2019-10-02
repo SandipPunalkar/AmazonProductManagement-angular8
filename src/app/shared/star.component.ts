706352c63db3ffbd3fbef09ba8184f86098f3c9e
Sandip Punalkar <sandippunalkar9@gmail.com>
2019-10-02 11:51:12 +0530

    Add product service.

--- src/app/shared/star.component.ts
+++ src/app/shared/star.component.ts
@@ -1,22 +1,27 @@
-import { Component, OnChanges, Input, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  OnChanges,
+  Input,
+  Output,
+  EventEmitter
+} from "@angular/core";
 
 @Component({
-  selector: 'pm-star',
-  templateUrl: './star.component.html',
-  styleUrls: ['./star.component.css']
+  selector: "pm-star",
+  templateUrl: "./star.component.html",
+  styleUrls: ["./star.component.css"]
 })
 export class StarComponent implements OnChanges {
-  @Input() rating:number;
-  starWidth:number;
+  @Input() rating: number;
+  starWidth: number;
 
-  @Output() ratingClicked: EventEmitter<string> =
-    new EventEmitter<string>();
+  @Output() ratingClicked: EventEmitter<string> = new EventEmitter<string>();
 
-  ngOnChanges():void {
-    this.starWidth = this.rating * 75/5;
+  ngOnChanges(): void {
+    this.starWidth = (this.rating * 75) / 5;
   }
 
-onClick(): void {
+  onClick(): void {
     this.ratingClicked.emit(`The rating ${this.rating} was clicked!`);
   }
 }
