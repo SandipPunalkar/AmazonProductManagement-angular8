eaba8cdbabd22c834f312478310a590c999a9ef1
Sandip Punalkar <sandippunalkar9@gmail.com>
2019-10-02 12:05:22 +0530

    Add the product service in product list.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
-import { Component } from '@angular/core';
+import { ProductService } from "./products/product.service";
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'pm-root',
-  template:`
+  selector: "pm-root",
+  template: `
     <div>
-    <h1>{{pageTitle}}</h1>
-    <pm-products></pm-products>
+      <h1>{{ pageTitle }}</h1>
+      <pm-products></pm-products>
     </div>
-    `
+  `,
+  providers: [ProductService]
 })
 export class AppComponent {
-  pageTitle:string = 'Aamzon product Management';
+  pageTitle: string = "Aamzon product Management";
 }
--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,3 +1,4 @@
+import { ProductService } from "./product.service";
 import { Component, OnInit } from "@angular/core";
 import { IProduct } from "./product";
 
@@ -19,46 +20,27 @@ export class ProductListComponent implements OnInit {
   }
   set listFilter(value: string) {
     this._listFilter = value;
-    this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+    this.filteredProducts = this.listFilter
+      ? this.performFilter(this.listFilter)
+      : this.products;
   }
 
   filteredProducts: IProduct[];
 
-  products: IProduct[] = [
-    {
-      productId: 1,
-      productName: "Leaf Rake",
-      productCode: "GDN-0011",
-      releaseDate: "March 19, 2019",
-      description: "Leaf rake with 48-inch wooden handle.",
-      price: 19.95,
-      starRating: 3.2,
-      imageUrl: "assets/images/leaf_rake.png"
-    },
-    {
-      productId: 2,
-      productName: "Garden Cart",
-      productCode: "GDN-0023",
-      releaseDate: "March 18, 2019",
-      description: "15 gallon capacity rolling garden cart",
-      price: 32.99,
-      starRating: 4.2,
-      imageUrl: "assets/images/garden_cart.png"
-    }
-  ];
-  constructor(){
-    this.filteredProducts = this.products;
-    this.listFilter ="cart";
-  }
+  products: IProduct[] = [];
+
+  constructor(private _productService: ProductService) {}
 
   onRatingClicked(message: string): void {
-    this.pageTitle = 'Product List: ' + message;
+    this.pageTitle = "Product List: " + message;
   }
 
   performFilter(filterBy: string): IProduct[] {
     filterBy = filterBy.toLocaleLowerCase();
-    return this.products.filter((product: IProduct) =>
-      product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
+    return this.products.filter(
+      (product: IProduct) =>
+        product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1
+    );
   }
 
   toggleImage(): void {
@@ -66,6 +48,7 @@ export class ProductListComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    console.log("In OnInit");
+    this.products = this._productService.getProducts();
+    this.filteredProducts = this.products;
   }
 }
