55c6b38bb6b04a34fe0304e178febdc1d2530eda
Sandip Punalkar <sandippunalkar9@gmail.com>
2019-10-02 13:01:15 +0530

    Add the http service product date

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ProductListComponent } from './products/product-list.component';
-import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
-import { StarComponent } from './shared/star.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { ProductListComponent } from "./products/product-list.component";
+import { ConvertToSpacesPipe } from "./shared/convert-to-spaces.pipe";
+import { StarComponent } from "./shared/star.component";
 
 @NgModule({
   declarations: [
@@ -15,12 +16,8 @@ import { StarComponent } from './shared/star.component';
     ConvertToSpacesPipe,
     StarComponent
   ],
-  imports: [
-    BrowserModule,
-    FormsModule,   
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, FormsModule, HttpClientModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
