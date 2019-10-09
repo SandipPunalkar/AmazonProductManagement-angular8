2d3d3b490c9f9b0129781f4e31b65640fc14ad7e
Sandip Punalkar <sandippunalkar9@gmail.com>
2019-10-09 21:57:02 +0530

    Add thems

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,20 +3,21 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: "pm-root",
-  template: `
-    <div>
-      <nav class="navbar navbar-default">
-        <div class="container-fluid">
-          <a class="navbar-brand">{{ pageTitle }}</a>
-          <ul class="nav navbar-nav">
-            <li><a [routerLink]="['/welcome']">Home</a></li>
-            <li><a [routerLink]="['/products']">Product List</a></li>
-          </ul>
-          <router-outlet></router-outlet>
-        </div>
-      </nav>
-    </div>
-  `,
+  templateUrl: "./app.component.html",
+  // template: `
+  //   <div>
+  //     <nav class="navbar navbar-default">
+  //       <div class="container-fluid">
+  //         <a class="navbar-brand">{{ pageTitle }}</a>
+  //         <ul class="nav navbar-nav">
+  //           <li><a [routerLink]="['/welcome']">Home</a></li>
+  //           <li><a [routerLink]="['/products']">Product List</a></li>
+  //         </ul>
+  //         <router-outlet></router-outlet>
+  //       </div>
+  //     </nav>
+  //   </div>
+  // `,
   providers: [ProductService]
 })
 export class AppComponent {
